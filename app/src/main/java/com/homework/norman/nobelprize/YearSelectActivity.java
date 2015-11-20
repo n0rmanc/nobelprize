c11716a90bc4dfb0a8eed7380f39ad3dddc8cc0c
Norman Chen <s965202030@gmail.com>
2015-11-20 15:22:36 +0800

    init

---
+++ app/src/main/java/com/homework/norman/nobelprize/YearSelectActivity.java
@@ -0,0 +1,13 @@
+package com.homework.norman.nobelprize;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class YearSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_year_select);
+    }
+}
